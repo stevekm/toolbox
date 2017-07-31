54c7721d7203869f5240118f7eff66209ec224c4
Stephen Kelly <stevekm@users.noreply.github.com>
2017-07-31 13:53:36 -0400

    Create toolbox.sh

---
+++ toolbox.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+spinner()
+{
+    # function for a cursor-spinner while a process is running
+    # Example:
+    # ( command ) &
+    # spinner
+    local pid=$!
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
