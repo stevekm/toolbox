c1e912e1a2597ba018ffec7088baf7ceb369ee7f
Stephen Kelly <stevekm@users.noreply.github.com>
2017-08-11 10:46:36 -0400

    add R sysinfo

--- toolbox.R
+++ toolbox.R
@@ -24,3 +24,31 @@ sort_bed_df <- function(df){
     return(df)
 }
 
+sysinfo <- function(){
+    # print custom information about the system
+
+        # check if 'mycat' is loaded in case I copy/pasted this from elsewhere
+    if( ! exists('mycat')) mycat <- function(text){cat(gsub(pattern = "\n", replacement = "  \n", x = text))}
+
+    # system info for use on Linux with GNU tools installed
+    # mycat(sprintf("System:\n%s\n%s", system("hostname", intern = TRUE), system("uname -srv", intern = TRUE)))
+    # mycat(sprintf("System user:\n%s", system("whoami", intern = TRUE)))
+    
+    # dir
+    # mycat(sprintf("System location:\n%s", system('pwd', intern = T, ignore.stderr = TRUE)))
+    mycat(sprintf("System location:\n%s", getwd()))
+    
+    
+    # repo info
+    mycat(sprintf("Git Remote:\n%s\n", system('git remote -v', intern=T)))
+    mycat(sprintf("Git branch and commit\n%s", system('printf "%s: %s" "$(git rev-parse --abbrev-ref HEAD)" "$(git rev-parse HEAD)"', 
+                                                      intern = TRUE,  ignore.stderr = TRUE)))
+    
+    # date time
+    mycat(sprintf("Time and Date of report creation:\n%s", system("date", intern = TRUE)))
+    
+    # R system info, packages, etc
+    print(sessionInfo())
+    print(Sys.info())
+}
+
