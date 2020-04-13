e5148ab82166d1ae22384066be902bdd28f68374
Stephen Kelly <stevekm@users.noreply.github.com>
2020-04-13 12:20:34 -0400

    add ssh script for LSF

--- LSF/clean_scratch.sh
+++ LSF/clean_scratch.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 # script for cleaning up the contents of the node scratch dir
-# need to use a regular bsub job if you do not have ssh access to the node
+# requires ssh access to nodes;
+
+# To ssh into juno or a node, you need ssh keys available. From your Mac, try:
+#
+# head -2 ~/.ssh/id_rsa
+# ssh-add -K ~/.ssh/id_rsa
+# ssh-add -L
+# ssh -vA silo
+#
+# Then from silo, try:
+# ssh-add -L
+# ssh ja06
+
+if [ "$USER" == "roslin" ]; then echo "dont run this as user roslin"; exit 1; fi
+if [ "$USER" == "voyager" ]; then echo "dont run this as user voyager"; exit 1; fi
 
 machines=$(bhosts | egrep "(^j.[0-9]+)" | grep -v unavail | awk '{print $1}')
 
 for machine in $machines; do
-    echo "rm -rf /scratch/$USER; mkdir -p /scratch/$USER" | \
-    bsub \
-    -n 1 \
-    -sla CMOPI \
-    -W 00:10 \
-    -o $PWD/${machine}.%J.out.log \
-    -e $PWD/${machine}.%J.err.log \
-    -R "select[hname==$machine]"
+    command="echo $machine; rm -rf /scratch/$USER; mkdir -p /scratch/$USER"
+    ssh $machine $command
 done
